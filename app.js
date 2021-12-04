f66288fbede9e042b15a4e1b9fde0efd64d8be22
David Blandon <90638931+davidbb10@users.noreply.github.com>
2021-12-04 09:10:50 -0500

    CAMBIOS

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ mongoose = require("mongoose");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(__dirname +'/public'))
 app.use(require('./routers/router'));
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve('./public/index.html'));
-})
+
 // Configurar cabeceras y cors
 
 
